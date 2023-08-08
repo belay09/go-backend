1ec97a7c054652543de946a2f284621cd0d6d30f
bete7512 <betekbebe@gmail.com>
2023-08-08 14:25:53 +0300

    Autgit push

---
+++ automation/console.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+cd ../../Client\ Projects/Zonner-Backend/hasura && hasura console --admin-secret="OlfYeenaXko46zqI89K7N1rK43"
--- routes/router.go
+++ routes/router.go
@@ -3,13 +3,10 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"go_backend/actions"
-	"net/http"
 )
 
 func InitializeRoutes(router *gin.RouterGroup) {
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "Hello, world!"})
-	})
+
 	router.POST("/login", actions.Login)
 	router.GET("/test", actions.Test)
 	router.POST("/customer_signup", actions.CustomerSignup)
